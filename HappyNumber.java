2aa3024b4f2020ccfd1c92dcb940d745658891ca
Jessica <jess.lam948@gmail.com>
2020-04-02 15:10:52 -0700

    Removed hard code
    Originally hard coded a counter to end the loop and indicate if the number is "not happy". Realized it wasn't a viable solution and changed it.

--- HappyNumber.java
+++ HappyNumber.java
@@ -2,16 +2,17 @@ class Solution {
     public boolean isHappy(int n) {
         int temp = 0;
         int ans = n;
-        int counter = 0;
+        HashSet seen = new HashSet();
+        boolean isNotLoop = true;
         
-        while (ans != 1 && counter < 10) {
+        while (ans != 1 && isNotLoop) {
+            isNotLoop = seen.add(new Integer(ans));
             while (ans > 0) {
                 temp += (ans % 10) * (ans % 10);
                 ans /= 10;
             }
             ans = temp;
             temp = 0;
-            counter++;
         }
         
         if (ans == 1) {
