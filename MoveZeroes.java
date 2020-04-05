d16cd6d65a4d4aad27617f530be53586396626fa
Jessica <jess.lam948@gmail.com>
2020-04-04 17:21:02 -0700

    Solved Leetcode : Move Zeroes
    https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3286/

---
+++ MoveZeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZeroInd = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroInd] = nums[i];
+                nonZeroInd++;
+            }
+        }
+        for (int j = nonZeroInd; j < nums.length; j++) {
+            nums[j] = 0;
+        }
+    }
+}
