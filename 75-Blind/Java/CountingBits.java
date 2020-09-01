008c10640b257b6061512023698caf2affa4d811
Jessica <jess.lam948@gmail.com>
2020-08-31 21:46:16 -0700

    Create CountingBits.java

---
+++ 75-Blind/Java/CountingBits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] countBits(int num) {
+        int[] result = new int[num + 1];
+        
+        for (int i = 1; i < result.length; i++) {
+            if (i % 2 == 1) {
+                result[i] = result[i >> 1] + 1;
+            } else {
+                result[i] = result[i >> 1];
+            }
+        }
+        
+        return result;
+    }
+}
