7ab4d1f57ab2010c113cfc5fef911b750daccfee
Jessica <jess.lam948@gmail.com>
2020-09-03 21:57:23 -0700

    Create ReverseBits.java

---
+++ 75-Blind/Java/ReverseBits.java
@@ -0,0 +1,16 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int reverse = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            reverse = reverse << 1;
+            if ((n & 1) == 1) {
+                reverse ^= 1;
+            }
+            n = n >>> 1;
+        }
+        
+        return reverse;
+    }
+}
